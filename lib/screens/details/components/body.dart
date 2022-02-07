a23a9261414c7507301faf8e0319ad9d3f1dbe79
Shazaib-Ahmed <shazzy1901.sa@gmail.com>
2022-02-07 21:21:56 +0530

    commit

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/details/components/icon_card.dart';
+import 'package:plant_app/screens/details/components/image_and_icons.dart';
+import 'package:plant_app/screens/details/components/title_and_price.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ImageAndIconsCard(size: size),
+          const TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: FlatButton(
+                  shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20),
+                  )),
+                  color: kPrimaryColor,
+                  onPressed: () {},
+                  child: const Text(
+                    "Buy Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: FlatButton(
+                onPressed: () {},
+                child: Text("Description"),
+              ))
+            ],
+          ),
+          // SizedBox(
+          //   height: kDefaultPadding * 2,
+          // )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/components/title_with_more_btn.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:plant_app/constants.dart';
+
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  final String title;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          TitleWithCustomUnderline(text: title),
+          Spacer(),
+          FlatButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            color: kPrimaryColor,
+            onPressed: press(),
+            child: const Text(
+              "More",
+              style: TextStyle(color: Colors.green, fontFamily: 'Poppins'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomUnderline extends StatelessWidget {
+  const TitleWithCustomUnderline({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Poppins'),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: const EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
